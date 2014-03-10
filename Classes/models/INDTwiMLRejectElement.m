167c882778441c4b5102133c03ed1666a61be138
Jeff Styles <jstyles@indatus.com>
2014-03-10 12:46:29 -0400

    Implementation of Reject/Pause models

--- Classes/models/INDTwiMLRejectElement.m
+++ Classes/models/INDTwiMLRejectElement.m
@@ -1,13 +1,47 @@
 //
 //  INDTwiMLRejectElement.m
-//  Airbreak
 //
-//  Created by Jeff Trespalacios on 3/10/14.
+//  Created by Jeff Styles on 3/10/14.
 //  Copyright (c) 2014 Jeff Trespalacios. All rights reserved.
 //
 
 #import "INDTwiMLRejectElement.h"
 
 @implementation INDTwiMLRejectElement
+
+- (instancetype)initWithTagName:(NSString*)tagName andValue:(NSString*)value
+{
+    self = [super initWithTagName:tagName
+                         andValue:value];
+
+    if (self) {
+        _reason = TwiMLRejectReasonRejected;
+    }
+
+    return self;
+}
+
+- (NSString*)xmlString
+{
+    return [self xmlStringForTag:self.tagName
+                  withAttributes:@{
+                      @"reason" : [self reasonString]
+                  }
+                        andValue:self.value];
+}
+
+- (NSString*)reasonString
+{
+    switch (_reason) {
+    case TwiMLRejectReasonBusy:
+        return @"busy";
+        break;
+    case TwiMLRejectReasonRejected:
+        return @"rejected";
+    default:
+        return @"rejected";
+        break;
+    }
+}
 
 @end
