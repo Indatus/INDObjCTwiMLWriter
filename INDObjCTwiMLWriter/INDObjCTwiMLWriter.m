327256ace79b12af712fee593cfd5c04defe6b79
Jeff Trespalacios <jeff.trespalacios@gmail.com>
2014-03-10 13:50:24 -0400

    reorganize and use project

---
+++ INDObjCTwiMLWriter/INDObjCTwiMLWriter.m
@@ -0,0 +1,13 @@
+//
+//  INDObjCTwiMLWriter.m
+//  INDObjCTwiMLWriter
+//
+//  Created by Jeff Trespalacios on 3/10/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import "INDObjCTwiMLWriter.h"
+
+@implementation INDObjCTwiMLWriter
+
+@end
---
+++ INDObjCTwiMLWriterTests/INDObjCTwiMLWriterTests.m
@@ -0,0 +1,34 @@
+//
+//  INDObjCTwiMLWriterTests.m
+//  INDObjCTwiMLWriterTests
+//
+//  Created by Jeff Trespalacios on 3/10/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface INDObjCTwiMLWriterTests : XCTestCase
+
+@end
+
+@implementation INDObjCTwiMLWriterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
