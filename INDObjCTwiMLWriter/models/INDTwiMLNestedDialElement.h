a8a97ead44bf86554bbe51eb89774605153a0058
Jeff Styles <jstyles@indatus.com>
2014-03-11 10:35:13 -0400

    NestedDial.h/.m implemented

--- INDObjCTwiMLWriter/models/INDTwiMLNestedDialElement.h
+++ INDObjCTwiMLWriter/models/INDTwiMLNestedDialElement.h
@@ -8,6 +8,18 @@
 
 #import "INDTwiMLNestableElement.h"
 
+typedef NS_ENUM(NSInteger, TwiMLHTTPMethod) {
+    TwiMLHTTPMethodPOST,
+    TwiMLHTTPMethodGET
+};
+
 @interface INDTwiMLNestedDialElement : INDTwiMLNestableElement
 
+@property (nonatomic) NSString* action;
+@property (nonatomic) TwiMLHTTPMethod method;
+@property (nonatomic) NSUInteger timeout;
+@property (nonatomic) BOOL hangupOnStar;
+@property (nonatomic) NSUInteger timeLimit;
+@property (nonatomic) NSString* callerId;
+@property (nonatomic) BOOL record;
 @end
--- INDObjCTwiMLWriter/models/INDTwiMLNestedDialElement.m
+++ INDObjCTwiMLWriter/models/INDTwiMLNestedDialElement.m
@@ -10,4 +10,69 @@
 
 @implementation INDTwiMLNestedDialElement
 
+- (instancetype)initWithTagName:(NSString*)tagName
+{
+    self = [super initWithTagName:tagName];
+    if (self) {
+        _method = TwiMLHTTPMethodPOST;
+        _timeout = 30;
+        _hangupOnStar = NO;
+        _timeLimit = 14400;
+        _record = NO;
+    }
+
+    return self;
+}
+
+- (NSDictionary*)attributes
+{
+    NSMutableDictionary* dict = [NSMutableDictionary new];
+    if (_action) {
+        dict[@"action"] = _action;
+    }
+
+    dict[@"method"] = [self methodString];
+    dict[@"timeout"] = [NSString stringWithFormat:@"%lud", (unsigned long)_timeout];
+    dict[@"hangupOnStar"] = [self boolToString:_hangupOnStar];
+    dict[@"timeLimit"] = [NSString stringWithFormat:@"%lud", (unsigned long)_timeLimit];
+
+    if (_callerId) {
+        dict[@"caller_id"] = _callerId;
+    }
+
+    dict[@"record"] = [self boolToString:_record];
+
+    return [dict copy];
+}
+
+#pragma mark - Private
+- (NSString*)methodString
+{
+    switch (_method) {
+    case TwiMLHTTPMethodGET:
+        return @"GET";
+        break;
+    case TwiMLHTTPMethodPOST:
+        return @"POST";
+        break;
+    default:
+        return @"POST";
+        break;
+    }
+}
+
+- (NSString*)boolToString:(BOOL)caseBool
+{
+    switch (caseBool) {
+    case NO:
+        return @"false";
+        break;
+    case YES:
+        return @"true";
+        break;
+    default:
+        return @"false";
+        break;
+    }
+}
 @end
