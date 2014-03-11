4197296f384244bb2643fa58cdbf5d3568d8e502
Jeff Styles <jstyles@indatus.com>
2014-03-11 08:32:17 -0400

    Implemented INDTwiMLElement.h

--- INDObjCTwiMLWriter/models/INDTwiMLRecordElement.h
+++ INDObjCTwiMLWriter/models/INDTwiMLRecordElement.h
@@ -1,13 +1,25 @@
 //
 //  INDTwiMLRecordElement.h
-//  Airbreak
 //
-//  Created by Jeff Trespalacios on 3/10/14.
+//  Created by Jeff Styles on 3/10/14.
 //  Copyright (c) 2014 Jeff Trespalacios. All rights reserved.
 //
 
 #import "INDTwiMLElement.h"
 
+typedef NS_ENUM(NSInteger, TwiMLHTTPMethod) {
+    TwiMLHTTPMethodPOST,
+    TwiMLHTTPMethodGET
+};
+
 @interface INDTwiMLRecordElement : INDTwiMLElement
 
+@property (nonatomic) NSString* action;
+@property (nonatomic) TwiMLHTTPMethod method;
+@property (nonatomic) NSUInteger timeout;
+@property (nonatomic) NSString* finishOnKey;
+@property (nonatomic) NSUInteger maxLength;
+@property (nonatomic) BOOL transcribe;
+@property (nonatomic) NSString* transcribeCallback;
+@property (nonatomic) BOOL playBeep;
 @end
