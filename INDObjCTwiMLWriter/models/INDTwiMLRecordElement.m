6adfbedb67dfda834a61c35f6eb4aba2ccf97eef
Jeff Styles <jstyles@indatus.com>
2014-03-11 08:47:14 -0400

    Implemented INDTwiMLRecordElement

--- INDObjCTwiMLWriter/models/INDTwiMLRecordElement.m
+++ INDObjCTwiMLWriter/models/INDTwiMLRecordElement.m
@@ -1,8 +1,7 @@
 //
 //  INDTwiMLRecordElement.m
-//  Airbreak
 //
-//  Created by Jeff Trespalacios on 3/10/14.
+//  Created by Jeff Styles on 3/10/14.
 //  Copyright (c) 2014 Jeff Trespalacios. All rights reserved.
 //
 
@@ -10,4 +9,90 @@
 
 @implementation INDTwiMLRecordElement
 
+- (instancetype)initWithTagName:(NSString*)tagName
+{
+    self = [super initWithTagName:tagName];
+
+    if (self) {
+        _method = TwiMLHTTPMethodPOST;
+        _timeout = 5;
+        _finishOnKey = @"123456789*#";
+        _maxLength = 3600;
+        _transcribe = NO;
+        _playBeep = YES;
+    }
+
+    return self;
+}
+
+- (NSString*)xmlString
+{
+    NSMutableDictionary* dict = [NSMutableDictionary new];
+    if (_action) {
+        dict[@"action"] = _action;
+    }
+
+    dict[@"method"] = [self methodString];
+    dict[@"timeout"] = [NSString stringWithFormat:@"%lud", (unsigned long)_timeout];
+    dict[@"finishOnKey"] = _finishOnKey;
+    dict[@"maxLength"] = [NSString stringWithFormat:@"%lud", (unsigned long)_maxLength];
+    dict[@"transcribe"] = [self transcribeString];
+
+    if (_transcribeCallback) {
+        dict[@"transcribeCallback"] = _transcribeCallback;
+    }
+
+    dict[@"playBeep"] = [self playBeepString];
+
+    return [self xmlStringForTag:self.tagName
+                  withAttributes:dict];
+}
+
+#pragma mark - Private
+- (NSString*)methodString
+{
+    switch (_method) {
+    case TwiMLHTTPMethodGET:
+        return @"GET";
+        break;
+    case TwiMLHTTPMethodPOST:
+        return @"POST";
+        break;
+    default:
+        return @"POST";
+        break;
+    }
+}
+
+- (NSString*)transcribeString
+{
+    switch (_transcribe) {
+    case NO:
+        return @"false";
+        break;
+
+    case YES:
+        return @"true";
+
+    default:
+        return @"false";
+        break;
+    }
+}
+
+- (NSString*)playBeepString
+{
+    switch (_playBeep) {
+    case NO:
+        return @"false";
+        break;
+
+    case YES:
+        return @"true";
+
+    default:
+        return @"false";
+        break;
+    }
+}
 @end
